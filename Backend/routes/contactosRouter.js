0ed1f0292d0fb9e9dfd58ab87c6c07708e50b86f
Antonio Branco <antoniobranco@sapo.pt>
2021-01-25 17:49:51 +0000

    proxy

--- Backend/routes/contactosRouter.js
+++ Backend/routes/contactosRouter.js
@@ -14,6 +14,7 @@ router.get('/', (req, res) => {
         (err, results) => {
             if (err) {
                 res.status(400).send('Query Error')
+                console.log(err)
             } else {
                 res.status(200).json(results);
             }
